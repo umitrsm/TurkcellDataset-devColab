7fe19100e8c1d01558469431219235a844f1cdcd
umitsm <umit.rsm.4@gmail.com>
2020-07-13 17:14:19 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/drawapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.drawapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/drawpage.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class DrawPage extends StatefulWidget {
+  DrawPage({Key key}) : super(key: key);
+
+  @override
+  _DrawPageState createState() => _DrawPageState();
+}
+
+class _DrawPageState extends State<DrawPage> {
+  List<Offset> _points = <Offset>[];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: GestureDetector(
+          onPanUpdate: (DragUpdateDetails details) {
+            setState(() {
+              RenderBox object = context.findRenderObject();
+              Offset _localPosition =
+                  object.globalToLocal(details.globalPosition);
+              _points = new List.from(_points)..add(_localPosition);
+            });
+          },
+          onPanEnd: (DragEndDetails details) => _points.add(null),
+          child: CustomPaint(
+            painter: Signature(points: _points),
+            size: Size.infinite,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.clear),
+        onPressed: () => _points.clear(),
+      ),
+    );
+  }
+}
+
+class Signature extends CustomPainter {
+  List<Offset> points;
+
+  Signature({this.points});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = new Paint()
+      ..color = Colors.blue
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 10.0;
+
+    for (int i = 0; i < points.length - 1; i++) {
+      if (points[i] != null && points[i + 1] != null) {
+        canvas.drawLine(points[i], points[i + 1], paint);
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(Signature oldDelegate) => oldDelegate.points != points;
+}
